6ee4b70f1337f551b2c5bc65dd91824dabe39974
Alexandr Korsak <alex.korsak@gmail.com>
2020-09-22 11:58:20 +0200

    start prototype test

--- tests/server/routes/storage.test.js
+++ tests/server/routes/storage.test.js
@@ -1,6 +1,20 @@
-describe('Sample Test', () => {
-  it('should test that true === true', () => {
-    expect(true).toBe(true)
-  })
-})
+const request = require('supertest')
+const app = require('../../../server')
 
+describe('Storage Endpoints', () => {
+    it('should be possible to create encrypted data record', async () => {
+        const id = 'example-id'
+
+        const res = await request(app)
+            .post('/storage/' + id + '/set')
+            .send({
+                encryption_key: 'super-key',
+                value: {
+                    key1: 'value1',
+                    key2: 'value2'
+                }
+            })
+
+        expect(res.statusCode).toEqual(201)
+    })
+})
